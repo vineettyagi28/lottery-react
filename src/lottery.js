39ae52fea70ea07358c91360af1c1eb63348370c
Vineet Tyagi <vineettyagi28@gmail.com>
2018-06-29 21:00:14 +0530

    Initial COmmit

---
+++ src/lottery.js
@@ -0,0 +1,47 @@
+import web3 from './web3';
+
+const abi = [{
+    "constant": true,
+    "inputs": [],
+    "name": "manager",
+    "outputs": [{"name": "", "type": "address"}],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "constant": false,
+    "inputs": [],
+    "name": "pickWinner",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+}, {
+    "constant": true,
+    "inputs": [],
+    "name": "getPlayers",
+    "outputs": [{"name": "", "type": "address[]"}],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "constant": false,
+    "inputs": [],
+    "name": "enter",
+    "outputs": [],
+    "payable": true,
+    "stateMutability": "payable",
+    "type": "function"
+}, {
+    "constant": true,
+    "inputs": [{"name": "", "type": "uint256"}],
+    "name": "players",
+    "outputs": [{"name": "", "type": "address"}],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+}, {"inputs": [], "payable": false, "stateMutability": "nonpayable", "type": "constructor"}];
+const address = '0x7dc3c9A20fff3047413cD6B61b41B302d1Ef2BCC';
+
+export default new web3.eth.Contract(abi, address);
+
